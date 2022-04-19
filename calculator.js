9d9ab25596d29beb90a0d4151a6871a64fd66f34
anupreetttt <anupreetpaulkar@gmail.com>
2022-04-19 03:43:24 -0500

    Body parse

--- calculator.js
+++ calculator.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function (req, res){
-    res.send("Hello world");
+    res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", function(req, res){
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var sum = num1 + num2;
+
+    res.send("Sum is: " + sum);
+})
 
 app.listen(3000, function(){
     console.log("Server is working");
